a0096ba6ded851cfc01784ebeac026e28d26bbb4
Adam Gardner <gardner.r.adam@gmail.com>
2024-03-07 20:57:21 +0100

    start

--- app/src/main/java/com/adamgardner/calorytracker/ui/theme/Type.kt
+++ app/src/main/java/com/adamgardner/calorytracker/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.plcoding.calorytrackerprep.ui.theme
+package com.adamgardner.calorytrackerprep.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
--- app/src/test/java/com/adamgardner/calorytracker/ExampleUnitTest.kt
+++ app/src/test/java/com/adamgardner/calorytracker/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.plcoding.calorytracker
+package com.adamgardner.calorytracker
 
 import org.junit.Test
 
